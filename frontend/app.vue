a245bbd60845f96f0a25c52375c7a720b99551ad
JakubLipnicki <jakublipnicki1@gmail.com>
2025-03-06 18:10:18 +0100

    [FE] init

---
+++ frontend/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+    <main class="flex content-center justify-center h-full">
+      <Button>Test</Button>
+    </main>
+  </div>
+</template>
