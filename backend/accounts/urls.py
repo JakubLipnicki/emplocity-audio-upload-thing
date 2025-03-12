f114e7e23932822354f5c93787dbdd9a9cca39db
SebastianTylicki <sebastian.tylicki@onet.pl>
2025-03-12 14:46:35 +0100

    Add user login

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LoginViev, UserView
+
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
-
+    path('login', LoginViev.as_view()),
+    path('user', UserView.as_view()),
 ]
