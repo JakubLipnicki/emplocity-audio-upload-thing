f7020f808f50a76a57f2c9b3c418cf8e74e3a2e0
SebastianTylicki <sebastian.tylicki@onet.pl>
2025-03-12 14:54:05 +0100

    Add user logout

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginViev, UserView
+from .views import RegisterView, LoginViev, UserView, LogoutView
 
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
     path('login', LoginViev.as_view()),
     path('user', UserView.as_view()),
+    path('logout', LogoutView.as_view()),
 ]
