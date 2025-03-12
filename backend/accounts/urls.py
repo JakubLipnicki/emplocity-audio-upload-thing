3264cc5c60b3580dd1f0fcd01606d7f88c9b07db
SebastianTylicki <sebastian.tylicki@onet.pl>
2025-03-12 14:46:35 +0100

    Add user login

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from .views import RegisterView, UserView, LogoutView, VerifyEmailView, LoginView, RequestPasswordResetView, ResetPasswordView
+from .views import RegisterView, LoginViev, UserView
+
 
 urlpatterns = [
-    path('register', RegisterView.as_view(), name='register'),
-    path('login', LoginView.as_view(), name='login'),
-    path('user', UserView.as_view(), name='user'),
-    path('logout', LogoutView.as_view(), name='logout'),
-    path('verify-email/', VerifyEmailView.as_view(), name='verify-email'),
-    path('request-password-reset/', RequestPasswordResetView.as_view(), name='request-password-reset'),
-    path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
+    path('register', RegisterView.as_view()),
+    path('login', LoginViev.as_view()),
+    path('user', UserView.as_view()),
 ]
-
