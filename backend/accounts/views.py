62b42229383f7f5026f6b5be718e6d3d62231c7a
SebastianTylicki <sebastian.tylicki@onet.pl>
2025-03-12 14:46:35 +0100

    Add user login

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,64 +3,19 @@ from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .models import User
-from django.core.mail import send_mail
-
-from .utils import generate_confirmation_token, confirm_token
-import jwt
 import datetime
-from decouple import config
-from rest_framework.permissions import AllowAny
+from django.conf import settings
+import jwt
 
-SECRET_KEY = config('SECRET_KEY')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        
-        token = generate_confirmation_token(user.id)
-        verify_link = f"http://127.0.0.1:8000/api/verify-email/?token={token}"
+        serializer.save()
+        return Response(serializer.data)
 
-        send_mail(
-            'Account Verification',
-            f'Click the link to activate your account: {verify_link}',
-            EMAIL_HOST_USER,
-            [user.email],
-            fail_silently=False,
-        )
-
-        return Response({'message': 'Registration successful. Check your email to activate your account.'})
-
-
-class VerifyEmailView(APIView):
-    def get(self, request):
-        token = request.GET.get('token')
-        if not token:
-            return Response({'error': 'Missing token'}, status=400)
-
-        user_id = confirm_token(token)
-        if not user_id:
-            return Response({'error': 'Invalid or expired token'}, status=400)
-
-        try:
-            user = User.objects.get(id=user_id)
-        except User.DoesNotExist:
-            return Response({'error': 'User not found'}, status=404)
-
-        if user.is_active:
-            return Response({'message': 'Account is already active'}, status=400)
-
-
-        user.is_active = True
-        user.save(update_fields=['is_active'])
-
-        return Response({'message': 'Account has been activated!'})
-
-
-
-class LoginView(APIView):
+class LoginViev(APIView):
     def post(self, request):
         email = request.data['email']
         password = request.data['password']
@@ -72,19 +27,17 @@ class LoginView(APIView):
         
         if not user.check_password(password):
             raise AuthenticationFailed('Incorrect password!')
-
-        if not user.is_active:
-            raise AuthenticationFailed('Account not activated. Please check your email.')
-
+        
         payload = {
             'id': user.id,
             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
             'iat': datetime.datetime.utcnow()
         }
 
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        token = jwt.encode(payload, settings.SECRET_KEY_JWT, algorithm='HS256')
 
         response = Response()
+
         response.set_cookie(key='jwt', value=token, httponly=True)
 
         response.data = {
@@ -92,7 +45,6 @@ class LoginView(APIView):
         }
 
         return response
-
 
 class UserView(APIView):
     def get(self, request):
@@ -102,7 +54,7 @@ class UserView(APIView):
             raise AuthenticationFailed('Unauthenticated')
         
         try:
-            payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+            payload = jwt.decode(token, settings.SECRET_KEY_JWT, algorithms=['HS256'])
         except jwt.ExpiredSignatureError:
             raise AuthenticationFailed('Unauthenticated')
         
@@ -110,13 +62,3 @@ class UserView(APIView):
         serializer = UserSerializer(user)
 
         return Response(serializer.data)
-    
-class LogoutView(APIView):
-    def post(self, request):
-        response = Response()
-        response.delete_cookie('jwt')
-        response.data = {
-            'message': 'Success'
-        }
-        return response
-    
--- backend/project/settings.py
+++ backend/project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-)jgx%u**f@=vtgzmrn-ck_i$hp@=12(87j%g=7j6@)&o#x016#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -130,12 +130,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+SECRET_KEY_JWT = os.getenv('SECRET_KEY_JWT', 'b2822ef5f420de907cfb85198d0581daa92eaf8d506de3db58dae4b1e505f27991a41c90c8b6054561db600b21af25fdc60125c4aeec17d9f8421e6c7c0209f6e6d87b991d327d589f5b155295ca571293cabeca4e46fa3d8f25d9eacf1923bec3bfa612622b96a2b1e060774e2a74ab172964f28b8534527f188916b85408de2bc5a06c333dca8bf53495c508292e52b36635a171d9f9037b7860591aebcc1daee3521461142d84b6bb3ff1a7bbd80f9805e60baa7338ab9f9e9dc628549de5745a934b2d1e086c9d0a578f6a2b3b4f1a463c3681ab9e67b412183e37a5aaff69dc979c47328dfc71d8e1838cd31a00affe54fd89628d73cd6e4b2c0bcc51ed')
+
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
