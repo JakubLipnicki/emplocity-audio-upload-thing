cfc7cdc7dfdee01e769041067247b9a5358c5c0e
SebastianTylicki <sebastian.tylicki@onet.pl>
2025-03-18 19:57:07 +0100

    [BE][2] Add email verification

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginViev, UserView, LogoutView
-
+from .views import RegisterView, UserView, LogoutView, VerifyEmailView, LoginView
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
-    path('login', LoginViev.as_view()),
+    path('login', LoginView.as_view()),
     path('user', UserView.as_view()),
     path('logout', LogoutView.as_view()),
+    path('verify-email/', VerifyEmailView.as_view(), name='verify-email'),
 ]
