75536878eec7bef5f9710e95a4911ad422ee7b26
JakubLipnicki <jakublipnicki1@gmail.com>
2025-03-25 19:50:51 +0100

    tailwind screens config and layouts config

---
+++ frontend/components/Navbar/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="w-full h-16 bg-white shadow">
+        <div class="container mx-auto h-full flex items-center px-4">
+            <h1>Navbar text</h1>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+};
+</script>
