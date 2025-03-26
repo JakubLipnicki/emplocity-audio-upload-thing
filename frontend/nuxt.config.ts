b251aa60134d44275342c0bbfbd730bce1619292
JakubLipnicki <jakublipnicki1@gmail.com>
2025-03-26 11:30:19 +0100

    registration thru form, redirect to profile page on success

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,20 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   typescript: {
-    strict: false
+    strict: false,
   },
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+
+  nitro: {
+    devProxy: {
+      "/api": {
+        target: "http://localhost:8000/api",
+        changeOrigin: true,
+      },
+    },
+  },
+
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+
   shadcn: {
     /**
      * Prefix for all the imported component
      */
-    prefix: '',
+    prefix: "",
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: './components/ui'
-  }
-})
+    componentDir: "./components/ui",
+  },
+});
