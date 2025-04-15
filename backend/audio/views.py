8135870bf68299800e598b507c1d68b327dfe4ac
SebastianTylicki <sebastian.tylicki@onet.pl>
2025-04-15 23:12:20 +0200

    [BE][9] Add endpoint to list 10 latest uploaded audio files

--- backend/audio/views.py
+++ backend/audio/views.py
@@ -1,8 +1,6 @@
 from rest_framework import generics
-
 from .models import AudioFile
 from .serializers import AudioFileSerializer
-
 
 class AudioFileUploadView(generics.ListCreateAPIView):
     queryset = AudioFile.objects.all()
@@ -10,3 +8,7 @@ class AudioFileUploadView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save()
+
+class LatestAudioFilesView(generics.ListAPIView):
+    queryset = AudioFile.objects.all().order_by('-uploaded_at')[:10]
+    serializer_class = AudioFileSerializer
