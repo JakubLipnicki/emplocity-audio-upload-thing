0b3c12535e5eaaf2b51d189176ccd5f2e7fa9b52
SebastianTylicki <sebastian.tylicki@onet.pl>
2025-05-19 00:48:38 +0200

    [BE][14] Add anonymous upload with public/private flag and public listing

--- backend/audio/serializers.py
+++ backend/audio/serializers.py
@@ -1,15 +1,14 @@
-from rest_framework import serializers
 from urllib.parse import urljoin
 from django.conf import settings
+from rest_framework import serializers
 from .models import AudioFile
 
 class AudioFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = AudioFile
-        fields = ["id", "uuid", "title", "description", "file", "uploaded_at"]
+        fields = ["id", "uuid", "title", "description", "file", "is_public", "uploaded_at"]
 
     def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        full_file_url = urljoin(settings.AUDIO_FILE_BASE_URL + "/", instance.file.name)
-        representation["file"] = full_file_url
-        return representation
+        rep = super().to_representation(instance)
+        rep["file"] = urljoin(settings.AUDIO_FILE_BASE_URL + "/", instance.file.name)
+        return rep
